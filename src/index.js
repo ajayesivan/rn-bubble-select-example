3f64b0e03cb153fb2b6641249ba895d1f53f17fd
Ajay Sivan <ajayesivan@gmail.com>
2022-03-16 03:17:57 +0530

    Select fav screen completed

--- src/index.js
+++ src/index.js
@@ -2,23 +2,28 @@ import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import React from 'react';
 import screens from './screens';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 
 const Stack = createNativeStackNavigator();
 
 const App = () => {
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName={screens.SelectFavorite.name}>
-        <Stack.Screen
-          name={screens.SelectFavorite.name}
-          component={screens.SelectFavorite.component}
-        />
-        <Stack.Screen
-          name={screens.FavoriteList.name}
-          component={screens.FavoriteList.component}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <Stack.Navigator
+          screenOptions={{headerShown: false}}
+          initialRouteName={screens.SelectFavorite.name}>
+          <Stack.Screen
+            name={screens.SelectFavorite.name}
+            component={screens.SelectFavorite.component}
+          />
+          <Stack.Screen
+            name={screens.FavoriteList.name}
+            component={screens.FavoriteList.component}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
   );
 };
 
--- src/screens/SelectFavoriteScreen/index.js
+++ src/screens/SelectFavoriteScreen/index.js
@@ -1,8 +1,80 @@
-import React from 'react';
-import {Text} from 'react-native';
+import React, {useState} from 'react';
+import {
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  Text,
+  SafeAreaView,
+  useWindowDimensions,
+  Button,
+} from 'react-native';
+import BubbleSelect, {Bubble} from 'react-native-bubble-select';
+
+const favorites = require('./favorites.json');
 
 const SelectFavoriteScreen = () => {
-  return <Text>Favorite</Text>;
+  const [selected, setSelected] = useState([]);
+  const {width, height} = useWindowDimensions();
+
+  return (
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.wrapper}>
+        <Text style={styles.title}>Choose 3 or more of your interests</Text>
+        <Text style={styles.text}>
+          Tap once on the artists you like, or twice on the ones you love. Press
+          and hold the ones you don't.
+        </Text>
+        <View style={styles.selectWrapper}>
+          <BubbleSelect
+            onSelect={bubble => setSelected([...selected, bubble.id])}
+            onDeselect={bubble => {
+              const newSelected = [...selected];
+              newSelected.splice(newSelected.indexOf(bubble.id), 1);
+              setSelected(newSelected);
+            }}
+            width={width}
+            height={height}>
+            {Object.keys(favorites).map(id => (
+              <Bubble
+                id={id}
+                text={favorites[id].name}
+                color="red"
+                fontSize={12}
+              />
+            ))}
+          </BubbleSelect>
+        </View>
+        <Button disabled={selected.length < 3} title="Confirm Interests" />
+      </View>
+    </SafeAreaView>
+  );
 };
+
+const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  wrapper: {
+    flex: 1,
+  },
+  title: {
+    marginHorizontal: 10,
+    marginTop: 20,
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'black',
+  },
+  text: {
+    marginHorizontal: 10,
+    marginTop: 5,
+    marginBottom: 20,
+    fontSize: 14,
+    color: 'grey',
+  },
+  selectWrapper: {
+    flex: 1,
+  },
+});
 
 export default SelectFavoriteScreen;
